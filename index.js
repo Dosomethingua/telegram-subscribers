66ab667649592ac3c85f08f8f69f80a91e86df17
Dosomethingua <dosomethingua@gmail.com>
2024-12-15 20:22:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 // Конфігурація токенів та ID
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const NOTION_API_KEY = process.env.NOTION_API_KEY;
+const NOTION_DATABASE_ID = process.env.NOTION_DATABASE_ID;
 
 // Ініціалізація Telegram бота та Notion клієнта
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: false });
@@ -14,20 +15,7 @@ const notion = new Client({ auth: NOTION_API_KEY });
 const channels = [
   { name: "Tiny Verse", link: "@tverseofficialchannel", pageId: "142c9b1febab803091caebd3f0d931ee" },
   { name: "Harvest Moon", link: "@nearharvestmoon", pageId: "14ac9b1febab80468b0fdb3365903ef3" },
-  { name: "Hot", link: "@hotonnear", pageId: "135c9b1febab80f88539e4b76c1a76b9" },
-  { name: "Pocket Rocket Game", link: "@pocket_rocket_game", pageId: "142c9b1febab8000aa95c97f380ffee0" },
-  { name: "PAWS", link: "@pawsupfam", pageId: "135c9b1febab805c854ccfcbc6cd19c0" },
-  { name: "Bums", link: "@bums_official", pageId: "14ac9b1febab802a93a7d8b8dab0cff9" },
-  { name: "Drops", link: "@EtherDrops_News", pageId: "11cc9b1febab806e891edd9f5c4629cf" },
-  { name: "Cats&Dogs", link: "@catsdogs_community", pageId: "11cc9b1febab80cc8fa9d664ec63d51e" },
-  { name: "OKX Racer", link: "@okx_racer_official_announcement", pageId: "11cc9b1febab8073be0cceae8d0dbed9" },
-  { name: "Telegram Apps Center", link: "@trendingapps", pageId: "14bc9b1febab805db3c8fe742df2d417" },
-  { name: "Blink", link: "@blink_en", pageId: "13ac9b1febab80679bc4d3c4dc370b1f" },
-  { name: "Gomble (EggDrop)", link: "@officialgomble", pageId: "00694fca2eea4101bfe26aad05c05a83" },
-  { name: "Coub", link: "@coubnews", pageId: "11cc9b1febab805abc33c2fc7bae1496" },
-  { name: "#Memhash", link: "@memhash", pageId: "14fc9b1febab80ceb8a0ed19a0b79f95" },
-  { name: "Not Pixel", link: "@notpixel_channel", pageId: "11ac9b1febab80478ee2f251932a4067" },
-  { name: "True", link: "@trueworld", pageId: "11ac9b1febab807a8271dd757e5fce27" },
+  // Додайте інші канали тут...
 ];
 
 // Функція для отримання кількості підписників каналу
@@ -44,14 +32,11 @@ async function getSubscribersCount(channel) {
 // Оновлення даних у Notion
 async function updateNotionDatabase(channel, count) {
   try {
-    const response = await notion.pages.create({
-      parent: { database_id: NOTION_DATABASE_ID },
+    const response = await notion.pages.update({
+      page_id: channel.pageId,
       properties: {
-        Name: {
-          title: [{ text: { content: channel.name } }],
-        },
         tgsubs1: {
-          number: count, // ВАЖЛИВО! Використовуйте "number" для властивостей типу Number
+          number: count, // ВАЖЛИВО! Використовуємо "number" для оновлення числового значення
         },
       },
     });
